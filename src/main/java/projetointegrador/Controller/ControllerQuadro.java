96899ea95882fc91b26652804e4281e6e400437b
unknown <fellipe.augusto01@gmail.com>
2023-11-27 22:25:53 -0300

    Alterações no código

--- src/main/java/projetointegrador/Controller/ControllerQuadro.java
+++ src/main/java/projetointegrador/Controller/ControllerQuadro.java
@@ -3,36 +3,55 @@ package projetointegrador.Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import projetointegrador.Model.Entities.Acao;
 import projetointegrador.Model.Entities.Quadro;
 import projetointegrador.visual.HelloApplication;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
-public class ControllerQuadro implements Initializable {
-
-
+public class ControllerQuadro {
 
     private Quadro quadro;
 
+    private int index;
+
 
     @FXML
-    private SubScene acao01;
+    private  Label NomeProjeto;
+
+
+    @FXML
+    private ScrollPane aFazer;
+
+
+    private Pane paneAFazer = new Pane();
+
 
     protected void atualizaQuadro(){
 
-
-
     }
 
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    public void initialize(Quadro quadro) {
+        quadro = getQuadro();
+       // generateActions();
+        NomeProjeto.setText(quadro.retornaProjeto().get(index).getNome());
 
     }
 
@@ -46,9 +65,137 @@ public class ControllerQuadro implements Initializable {
     @FXML
     protected void addAtv(){
 
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("janelaCadastroAtividade.fxml"));
+            Parent root = loader.load();
+
+            CadastroAtividade cadastroAtividade = loader.getController();
+            cadastroAtividade.setQuadro(quadro);
+
+            Stage novaJanela = new Stage();
+            novaJanela.setTitle("Cadastro de Atividades");
+            novaJanela.setScene(new Scene(root));
+            novaJanela.showAndWait();
+
+            quadro = cadastroAtividade.getQuadro();
+            System.out.println(quadro.retornaProjeto().get(0).retornaAtividade().size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @FXML
     protected void addAcao(){
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("janelaCadastro.fxml"));
+            Parent root = loader.load();
+
+            CadastroAcao cadastroAcao = loader.getController();
+            cadastroAcao.setQuadro(quadro);
+
+            Stage novaJanela = new Stage();
+            novaJanela.setTitle("Cadastro de Ações");
+            novaJanela.setScene(new Scene(root));
+            novaJanela.showAndWait();
+
+            quadro = cadastroAcao.getQuadro();
+            System.out.println(quadro.retornaProjeto().get(0).retornaAtividade().size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    private void generateActions(){
+
+
+        int x = 0;
+        int y = 0;
+
+        paneAFazer.setPrefWidth(200);
+
+        for(int projeto = 0; projeto< quadro.retornaProjeto().size(); projeto++){
+
+            for (int atividade  = 0; atividade< quadro.retornaProjeto().get(projeto).retornaAtividade().size(); atividade++){
+
+                for (int acao = 0 ; acao < quadro.retornaProjeto().get(projeto).retornaAtividade().get(atividade).retornaAção().size(); acao++){
+                    paneAFazer.setMaxWidth(y);
+                    paneAFazer.getChildren().add(paneAcao((Acao) quadro.retornaProjeto().get(projeto).retornaAtividade().get(atividade).retornaAção().get(acao),x, y,quadro.retornaProjeto().get(projeto).retornaAtividade().get(atividade).getNome()));
+                    y = y + 100;
+                }
+            }
+        }
+
+        aFazer.setContent(paneAFazer);
+    }
+
+    public Pane paneAcao(Acao acao, int x, int y, String nomeAtividade){
+
+        Pane pane = new Pane();
+        pane.setLayoutX(x);
+        pane.setLayoutY(y);
+        pane.setPrefHeight(95);
+        pane.setPrefWidth(200);
+
+        Button button = new Button();
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Arial Black.ttf"), 12);
+        button.setBackground(Background.fill(Paint.valueOf("yellow")));
+        button.setStyle("-fx-background-radius: 20px");
+        button.setStyle("-fx-font-family: ArialBlack");
+        button.prefHeight(173);
+        button.prefWidth(71);
+        button.setPrefSize(173,71);
+        button.setLayoutX(10);
+        button.setLayoutY(19);
+        button.setStyle("-fx-border-color:  #000000");
+
+
+        Label nomeResponsa = new Label();
+        nomeResponsa.setLayoutX(17);
+        nomeResponsa.setLayoutY(24);
+        nomeResponsa.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 12));
+        Label dataIn = new Label();
+        dataIn.setLayoutX(16);
+        dataIn.setLayoutY(45);
+        Label dataOut = new Label();
+        dataOut.setLayoutX(83);
+        dataOut.setLayoutY(45);
+        Label status = new Label();
+        status.setLayoutX(16);
+        status.setLayoutY(67);
+        Label atividade = new Label(nomeAtividade);
+        atividade.setLayoutX(14);
+        atividade.setLayoutY(0);
+        nomeResponsa.setText(acao.getNome()+" - " +acao.getResponsavel());
+        dataIn.setText("In: "+acao.retornaStringInicio());
+        dataOut.setText("Out: "+acao.retornaStringFim());
+
+        if(acao.getDataDeTermino().isBefore(LocalDate.now())){
+            status.setText("No prazo...");
+        }
+        else {
+            status.setText("Atrasado");
+            status.setTextFill(Paint.valueOf("red"));
+
+        }
+
+        pane.getChildren().add(button);
+        pane.getChildren().add(nomeResponsa);
+        pane.getChildren().add(dataIn);
+        pane.getChildren().add(dataOut);
+        pane.getChildren().add(status);
+        pane.getChildren().add(atividade);
+
+
+        return pane;
+
 
     }
+
 }
